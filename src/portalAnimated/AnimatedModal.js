aca26a7cd2f984a3e58ee1774d9d41985dba62ba
tranzystor <tranzystor.github@gmail.com>
2018-05-02 17:45:34 +0200

    Animated modal

--- src/portalAnimated/AnimatedModal.js
+++ src/portalAnimated/AnimatedModal.js
@@ -1,8 +1,46 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
+import { Transition } from "react-spring";
+
+const modalRoot = document.getElementById("modal-root");
+
+const ModalBackgroundStyle = styled.div`
+  background-color: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 class AnimatedModal extends Component {
   render() {
-    return <div>AnimatedModal</div>;
+    const items = this.props.isVisible ? ["modal"] : [];
+    const children = (
+      <Transition
+        keys={items}
+        from={{ opacity: 0, scale: 0.7 }}
+        enter={{ opacity: 1, scale: 1 }}
+        leave={{ opacity: 0, scale: 0.7 }}
+      >
+        {items.map(item => styles => {
+          const { scale, ...rest } = styles;
+          return (
+            <ModalBackgroundStyle style={rest}>
+              <div style={{ transform: `scale(${scale})` }}>
+                {this.props.children}
+              </div>
+            </ModalBackgroundStyle>
+          );
+        })}
+      </Transition>
+    );
+
+    return ReactDOM.createPortal(children, modalRoot);
   }
 }
 
--- src/portalAnimated/AppWithModal.js
+++ src/portalAnimated/AppWithModal.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Modal from "./Modal";
+import AnimatedModal from "./AnimatedModal";
 import styled from "styled-components";
 
 const ModalBody = styled.div`
@@ -16,34 +16,28 @@ class AppWithModal extends Component {
   }
 
   handleShow() {
-    console.log("handle show");
     this.setState({ showModal: true });
   }
 
   handleHide() {
-    console.log("handle hide");
     this.setState({ showModal: false });
   }
 
   render() {
-    const modalBody = (
-      <ModalBody>
-        <div>
-          With a portal, we can render content into a different part of the DOM,
-          as if it were any other React child.
-        </div>
-        This is being rendered inside the #modal-container div.
-        <button onClick={this.handleHide}>Hide modal</button>
-      </ModalBody>
-    );
-
-    console.log("showModal: " + this.state.showModal);
     return (
-      <div>
-        This div has overflow: hidden.
+      <React.Fragment>
         <button onClick={this.handleShow}>Show modal</button>
-        <Modal isVisible={this.state.showModal}>{modalBody}</Modal>
-      </div>
+        <AnimatedModal isVisible={this.state.showModal}>
+          <ModalBody>
+            <div>
+              With a portal, we can render content into a different part of the
+              DOM, as if it were any other React child.
+            </div>
+            This is being rendered inside the #modal-container div.
+            <button onClick={this.handleHide}>Hide modal</button>
+          </ModalBody>
+        </AnimatedModal>
+      </React.Fragment>
     );
   }
 }
